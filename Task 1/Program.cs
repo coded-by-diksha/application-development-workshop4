bc2e4bcc790334a681407cc63126b6d4e0bf9098
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-11-19 11:43:01 +0545

    task 1 done

---
+++ Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_1
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student std1 = new student();
+            student std2= new student();
+            std1.id = 1;
+            std1.name = "John";
+            std1.age = 20;
+
+            std2.id = 2;
+            std2.name = "Alice";
+            std2.age = 22;
+            student.course = "BSC(hons) Computing";
+
+            Console.WriteLine("Student 1:");
+            Console.WriteLine($"ID: {std1.id}, Name: {std1.name}, Age: {std1.age}, Course: {student.course}");
+            Console.WriteLine("Student 2:");
+            Console.WriteLine($"ID: {std2.id}, Name: {std2.name}, Age: {std2.age}, Course: {student.course}");
+
+        }
+    }
+}
---
+++ Task 1/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class student
+    {
+        public int id;
+        public string name;
+        public int age;
+        public static string course;
+        
+    }
+}
