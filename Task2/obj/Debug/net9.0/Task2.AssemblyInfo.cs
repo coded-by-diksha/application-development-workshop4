aeedc04d4ccf5d7370859e96b87abd529aaaeeaf
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-11-19 21:40:10 +0545

    Task 6 done

--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42746fb30dd846c4b25ddb06edb975b8ea128b6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3769bd39184612d97b9f234355a8779c6803490e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
