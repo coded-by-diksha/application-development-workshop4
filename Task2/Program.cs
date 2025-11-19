08a767917f04a7c04d3adabb598631d187dcc794
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-11-19 11:56:10 +0545

    task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Calculator.PrintWelcome();
+              int sum = Calculator.Add(5, 10);
+                Console.WriteLine($"Sum: {sum}");
+                int product = Calculator.Multiply(5);
+                Console.WriteLine($"Product: {product}");
+            
+        }
+    }
+}
